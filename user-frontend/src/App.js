60be215c943bdbe9c4a8bd21d34a08c5c417e0cb
Sinchana Devadiga <sinchananiranjan6@gmail.com>
2026-04-15 12:40:25 +0530

    Cart management User Interface

--- user-frontend/src/App.js
+++ user-frontend/src/App.js
@@ -1,11 +1,22 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import SignupPage from './pages/user/SignupPage';
+import CartPage from './pages/cart/CartPage';
+// add more pages later
 
 function App() {
- return (
-    <div className="App">
-      <SignupPage />
-    </div>
+  return (
+    <Router>
+      <Routes>
+        
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/cart" element={<CartPage />} />
+        {/* Add others' pages like this */}
+        {/* <Route path="/login" element={<LoginPage />} /> */}
+        {/* <Route path="/products" element={<ProductPage />} /> */}
+      </Routes>
+    </Router>
   );
 }
 
